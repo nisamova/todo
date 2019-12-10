b50e1222759df619bd831872cfecfabebc9d47d0
Nazokat Isamova <isamovanazokat@gmail.com>
2019-12-09 21:03:11 -0600

    debug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Auth from 'aws-amplify';
+import {Auth, API, graphqlOperation} from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
+import * as queries from './graphql/queries';
+import * as mutation from './graphql/mutations';
 Auth.configure(awsconfig);
+API.configure(awsconfig);
 
 function App() {
+
+  const allTodos = API.graphql(graphqlOperation(queries.listTodos));
+  console.log(allTodos);
+
+  const oneTodo = API.graphql(graphqlOperation(queries.getTodo, {id:"0342be07-1ce8-4a43-b744-422b4daa210a"}));
+  console.log(oneTodo);
+
+  const todo = {name:"app", description: "new todo"};
+  const newTodo = API.graphql(graphqlOperation(mutations.createTodo, {input: todo}));
+
   return (
     <div className="App">
       <header className="App-header">
